8b0cb802cc80538846701b4024360ef89d4ea210
Zafkiel1312 <Timstar131220@gmail.com>
2023-02-01 18:01:57 +0100

    Added Backend module and files

---
+++ MangaGuideBackend/src/main/kotlin/de/zafkiel1312/mangaguide/backend/MangaGuideBackendApplication.kt
@@ -0,0 +1,11 @@
+package de.zafkiel1312.mangaguide.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MangaGuideBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MangaGuideBackendApplication>(*args)
+}
---
+++ MangaGuideBackend/src/test/kotlin/de/zafkiel1312/mangaguide/backend/MangaGuideBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.zafkiel1312.mangaguide.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MangaGuideBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
