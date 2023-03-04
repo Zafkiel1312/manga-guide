bf909c22883c2ef2bc48026d6cfbe8e28edcb6ba
Zafkiel1312 <Timstar131220@gmail.com>
2023-03-04 20:53:49 +0100

    Base commit for pocketbase backend

---
+++ backend/pb_migrations/1677959193_created_mangas.js
@@ -0,0 +1,124 @@
+migrate((db) => {
+  const collection = new Collection({
+    "id": "bhjwaxxaem0u430",
+    "created": "2023-03-04 19:46:33.327Z",
+    "updated": "2023-03-04 19:46:33.327Z",
+    "name": "mangas",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "jixu0xbv",
+        "name": "title",
+        "type": "text",
+        "required": true,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "zy33khme",
+        "name": "author",
+        "type": "text",
+        "required": true,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "aur2dnnv",
+        "name": "publisher",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "v1jdhdtm",
+        "name": "releaseDate",
+        "type": "date",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "fhdhdays",
+        "name": "picture",
+        "type": "url",
+        "required": false,
+        "unique": false,
+        "options": {
+          "exceptDomains": null,
+          "onlyDomains": null
+        }
+      },
+      {
+        "system": false,
+        "id": "u3kgpdf7",
+        "name": "releasedVolumes",
+        "type": "number",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null
+        }
+      },
+      {
+        "system": false,
+        "id": "bq9gggdd",
+        "name": "ownedVolumes",
+        "type": "number",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null
+        }
+      },
+      {
+        "system": false,
+        "id": "6zuafgwo",
+        "name": "nextVolumeRelease",
+        "type": "date",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      }
+    ],
+    "listRule": "",
+    "viewRule": "",
+    "createRule": "",
+    "updateRule": "",
+    "deleteRule": "",
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("bhjwaxxaem0u430");
+
+  return dao.deleteCollection(collection);
+})
